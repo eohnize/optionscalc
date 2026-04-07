3a07cd1903597b39e5c196e80b6b84ea82791fd2
eohnize <onghz.edwin@gmail.com>
2026-04-07 23:52:54 +0800

    Integrate FastAPI with existing app

--- web-app/api/index.py
+++ web-app/api/index.py
@@ -1,1 +1,6 @@
-from options_calc_server import app
+from fastapi import FastAPI
+from options_calc_server import app as base_app
+
+app = FastAPI()
+app.mount("/api", base_app)
+app.mount("/", base_app)
